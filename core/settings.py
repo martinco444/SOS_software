504769544252879dd373fafad64bf58b5892a65f
Martin Correa <comartin910@gmail.com>
2025-09-11 16:33:15 -0500

    correccion imortacion #2

--- core/settings.py
+++ core/settings.py
@@ -12,18 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from python_decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='default-dev-only-key')
+
 DEBUG = 'RAILWAY_ENVIRONMENT' not in os.environ
+
 ALLOWED_HOSTS = ['*'] if 'RAILWAY_ENVIRONMENT' in os.environ else []
 
 # Application definition
